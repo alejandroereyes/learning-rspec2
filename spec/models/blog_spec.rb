2b5501d4efd94ba91bb832e3efbdcf3abbee04fc
Alex Reyes <alejandroereyes@gmail.com>
2015-09-18 07:25:50 -0500

    movd blog var into spec let var

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 
 describe Blog do
 
+  let(:blog) { Blog.new(title: "My Blog",
+            comments_feed_url: "http://example.com/comments/feed") }
+
   it "saves attr" do
-    blog = Blog.new(title: "My Blog",
-        comments_feed_url: "http://example.com/comments/feed")
     blog.save!
     expect(blog).to be_valid
   end
